c58e74aa55a69360e3d6bde4469a99f2fd5169ee
Ethan <ethan.t.schmidt@gmail.com>
2017-03-19 12:21:16 -0700

    asdf

--- app/models/bot.rb
+++ app/models/bot.rb
@@ -4,7 +4,7 @@ client = Twitter::REST::Client.new do |config|
   config.consumer_key        = ENV["CONSUMER_KEY"]
   config.consumer_secret     = ENV["CONSUMER_SECRET"]
   config.access_token        = ENV["ACCESS_TOKEN"]
-  config.access_token_secret = ENV["ACCESS_SECRET"]
+  config.access_token_secret = ENV["ACCESS_TOKEN_SECRET"]
 end
 
 wod = Wod.new
