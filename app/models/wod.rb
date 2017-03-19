6d7762b229cd162f2887819481178f9b139f2b2f
Ethan <ethan.t.schmidt@gmail.com>
2017-03-18 18:47:08 -0700

    kinda works now

--- app/models/wod.rb
+++ app/models/wod.rb
@@ -3,12 +3,26 @@ class Wod < ApplicationRecord
   def initialize
     @wod_type = WOD_TYPES.sample
     @time = time
+    @rounds = rounds
     @pull = PULLS.sample
 		@run = RUNS.sample
 		@sit = SITS.sample
 		@jump = JUMPS.sample
 		@light = LIGHTS.sample
 		@heavy = HEAVYS.sample
+    @movements = movements
+    @movement1 = @movements.at(0)
+    @movement2 = @movements.at(1)
+    @movement3 = @movements.at(2)
+    @movement4 = @movements.at(3)
+    @weight1 = weight(@movement1)
+    @weight2 = weight(@movement2)
+    @weight3 = weight(@movement3)
+    @weight4 = weight(@movement4)
+    @reps1 = reps
+    @reps2 = reps
+    @reps3 = reps
+    @reps4 = reps
   # @pull2 = PULLS.sample
 	#	@run2 = RUNS.sample
 	#	@sit2 = SIT.sample
@@ -38,19 +52,21 @@ HEAVYS = ["BS", "FS", "DL", "PJ", "PP", "Clean", "HC", "PC", "HPC", "C&J"]
     end
   end
 
+  def rounds
+    if @wod_type == "RFT"
+      return rand(1..7)
+    end
+  end
+
 # selects movements from categories
   def movements
-    @movements = [@pull, @run, @sit, @jump, @light, @heavy].shuffle
-    @movement1 = @movements.at(0)
-    @movement2 = @movements.at(1)
-    @movement3 = @movements.at(2)
-    @movement4 = @movements.at(3)
+    return [@pull, @run, @sit, @jump, @light, @heavy].shuffle
   end
 
 # if one of the movemnts is a light BB, then the heavy BB defaults to that weight as well
   def weight(movement)
   	if ["KBS", "KBSn", "KBC"].include? movement
-  		return rand(1, 1.5, 2) + "pd"
+  		return ["1", "1.5", "2"].sample + "pd"
   	elsif LIGHTS.include? movement
   	  return  rand(5..9) * 15
   	elsif HEAVYS.include? movement
@@ -61,44 +77,29 @@ HEAVYS = ["BS", "FS", "DL", "PJ", "PP", "Clean", "HC", "PC", "HPC", "C&J"]
   end
   	   	 
   def weights
-  	@weight1 = weight(@movement1)
-  	@weight2 = weight(@movement2)
-  	@weight3 = weight(@movement3)
-  	@weight4 = weight(@movement4)
+
   end
 
 # number of reps for each movement
   def reps
-    if @wod_type == "AMRAP"
-    	@rounds = ""
-      @reps1 = rand(3..20)
-      @reps2 = rand(3..20)
-      @reps3 = rand(3..20) 
-      @reps4 = rand(3..20)
-    elsif @wod_type == "RFT"
-    	@rounds = rand(1..7)
-      @reps1 = rand(3..20)
-      @reps2 = rand(3..20)
-      @reps3 = rand(3..20) 
-      @reps4 = rand(3..20)
-    else 
-    	@rounds = ""
-      @reps1 = rand(1..5)
-      @reps2 = rand(1..5)
+    if @wod_type == "EMOM"
+      return rand(1..5)
+    else
+      return rand(3..20)
     end
   end
 
   def print_wod
 		if @wod_type == "EMOM"  	
-    	"#{@wod_type}#{@time}\n" +
-    	"#{@reps1}" + "#{@movement1}" + "#{@weight1}\n" +
-    	"#{@reps2}" + "#{@movement2}" + "#{@weight2}\n"
+    	"#{@wod_type} #{@time}:\n" +
+    	"#{@reps1} " + "#{@movement1} " + "#{@weight1}\n" +
+    	"#{@reps2} " + "#{@movement2} " + "#{@weight2}\n"
     else
-    	"#{@rounds}#{@wod_type}#{@time}\n" +
-    	"#{@reps1}" + "#{@movement1}" + "#{@weight1}\n" +
-    	"#{@reps2}" + "#{@movement2}" + "#{@weight2}\n" +
-    	"#{@reps3}" + "#{@movement3}" + "#{@weight3}\n" +
-    	"#{@reps4}" + "#{@movement4}" + "#{@weight4}\n"
+    	"#{@rounds}#{@wod_type}#{@time}:\n" +
+    	"#{@reps1} " + "#{@movement1} " + "#{@weight1}\n" +
+    	"#{@reps2} " + "#{@movement2} " + "#{@weight2}\n" +
+    	"#{@reps3} " + "#{@movement3} " + "#{@weight3}\n" +
+    	"#{@reps4} " + "#{@movement4} " + "#{@weight4}\n"
     end
   end
 
